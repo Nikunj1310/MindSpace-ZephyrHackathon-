d2342d30a9f22cc8aacf7b9bc03c8ac8fe4a1e4d
Nikunj Goyal <nikunjgoyal1106@gmail.com>
2025-09-20 00:09:46 +0530

    inital setup

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const app = require('./src/app.js');
-const db = require('./src/config/database.js');
+// const db = require('./src/config/database.js');
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
