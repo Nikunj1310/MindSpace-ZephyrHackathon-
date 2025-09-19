4529240da3d31527e216ae2190776f637390743f
Nikunj Goyal <nikunjgoyal1106@gmail.com>
2025-09-20 00:26:32 +0530

    initial setup

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 require('dotenv').config();
 const app = require('./src/app.js');
-// const db = require('./src/config/database.js');
+const db = require('./src/config/database.js');
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
 });
+db.connectDB();
---
+++ src/config/database.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const connectDB = async() => {
+    try{
+        await mongoose.connect(process.env.MONGODB_URI).then(() => {
+            console.log(`Database connected successfully to ${process.env.MONGODB_URI}`);
+        });
+
+        // Set up event listeners on the connection
+        mongoose.connection.on('error', (err) => {
+            console.error('Database connection error:', err);
+        });
+        
+        mongoose.connection.on('disconnected', () => {
+            console.log('Database disconnected');
+        });
+        
+    }catch(err){
+        console.error('Database connection error:', err);
+        process.exit(1);
+    }
+}
+
+module.exports.connectDB = connectDB;
