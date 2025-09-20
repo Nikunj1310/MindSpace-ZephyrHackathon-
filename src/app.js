36ae447eae9cf65fd952920e8b0aaceb8c3eda03
Nikunj Goyal <nikunjgoyal1106@gmail.com>
2025-09-20 12:46:01 +0530

    bacnend

--- src/app.js
+++ src/app.js
@@ -7,4 +7,8 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+// User-related routes (register, login, update, delete, etc.)
+const userRoutes = require('./routes/auth.routes');
+app.use('/api/users', userRoutes);
+
 module.exports = app;
--- src/router/user.router.js
+++
@@ -1,5 +0,0 @@
-const userController = require('../controller/user.controller');
-const express = require('express');
-const router = express.Router();
-const { validateBody, validateParams } = require('../middleware/validation.middleware');
-const { userRegistrationSchema, userLoginSchema, userIdSchema, moodUpdateSchema, passwordChangeSchema } = require('../validation/user.validation');
